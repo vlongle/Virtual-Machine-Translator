ec85286b571957a612a084f2489d7d35e54f0f2e
vlongle <vietlong.lenguyen@gmail.com>
2018-07-03 12:54:05 +0700

    y
    y

--- src/VMTranslator.py
+++ src/VMTranslator.py
@@ -1,9 +1,16 @@
+import os
 
 class Translator:
-    def __init__(self, file_path):
+    def __init__(self, vmpath):
+        self.file_name = os.path.basename(vmpath).split('.')[0] # remove .vm extension
+        self.asm_path = os.path.join('..', 'output', self.file_name + '.asm')
+
 
     def run(self):
-        pass
+        self.vmfile = open(vmpath, 'r')
+        self.asmfile = open(self.asm_path, 'w')
+
+        lines = (line.split("//")[0].strip for line in self.vmfile if not line.isspace() )
 
     def exit(self):
         self.vmfile.close()

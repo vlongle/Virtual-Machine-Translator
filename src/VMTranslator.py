35a4791ea07a7830b8e92dd604980cc7ee8ade10
vlongle <vietlong.lenguyen@gmail.com>
2018-07-03 12:45:34 +0700

    modulation

---
+++ src/VMTranslator.py
@@ -0,0 +1,10 @@
+
+class Translator:
+    def __init__(self, file_path):
+        pass
+
+    def run(self):
+        pass
+
+    def exit(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,28 @@
+import time
+import os
+import sys
+from VMTranslator import Translator
+
+def main():
+#    try:
+        file_path = os.path.join('..', 'input', sys.argv[1])
+
+        translator = Translator(file_path)
+
+        print('Translating', sys.argv[1], 'the output will be in ./output')
+
+        start_time = time.time()
+        translator.run()
+        end_time = time.time()
+
+        translator.exit()
+
+        print('Finished translation in ', end_time - start_time)
+#    except:
+#        print("Please<Plug>PeepOpenut the .vm file in the ./input dir  and supply its name as the argument")
+#        raise SystemExit(1)
+#
+
+
+if __name__ == "__main__":
+    main()

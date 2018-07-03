c53d1cd85b5456a009e6261cd94dd6e862555f0e
vlongle <vietlong.lenguyen@gmail.com>
2018-07-03 12:48:10 +0700

    Y

--- src/VMTranslator.py
+++ src/VMTranslator.py
@@ -1,10 +1,10 @@
 
 class Translator:
     def __init__(self, file_path):
-        pass
 
     def run(self):
         pass
 
     def exit(self):
-        pass
+        self.vmfile.close()
+        self.asmfile.close()
